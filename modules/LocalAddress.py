0004e7f338aa90c9c890142744e45e18c11c0bc7
Yoshihiko Miyaichi <miyaichi@gmail.com>
2020-07-05 11:19:03 +0900

    Add network connection check and recovery

--- modules/LocalAddress.py
+++ modules/LocalAddress.py
@@ -21,34 +21,48 @@ def get_local_address():
 
 
 class LocalAddress(WeatherModule):
-    """Local IP Address
-    """
+    """Local IP address display and Network connection monitor module
 
+    This module can display the local ip address and also monitor the network connection.
+
+    example config:
+    {
+      "module": "LocalAddress",
+      "config": {
+        "rect": [x, y, width, height],
+        "seconds_to_reboot": 180
+       }
+    }
+    """
     def __init__(self, fonts, location, language, units, config):
         super().__init__(fonts, location, language, units, config)
-        self.retries = 0
-        self.max_retries = 60
+        self.seconds = 0
+        self.seconds_to_reboot = 0
+        if isinstance(config["seconds_to_reboot"], int):
+            self.seconds_to_reboot = config["seconds_to_reboot"]
 
     def draw(self, screen, weather, updated):
         if weather is None or not updated:
             return
 
         message = get_local_address()
+        if message:
+            self.seconds = 0
+        else:
+            message = "connection lost"
+            self.seconds += 1
+            if self.seconds_to_reboot and self.seconds > self.seconds_to_reboot:
+                Utils.reboot()
 
         self.clear_surface()
-        if message:
-            logging.info("%s: %s", __class__.__name__, message)
-            for size in ("large", "medium", "small"):
-                width, height = self.text_size(message, size, bold=True)
-                if width <= self.rect.width and height <= self.rect.height:
-                    break
-            self.draw_text(message, (0, 0),
-                           size,
-                           "white",
-                           bold=True,
-                           align="center")
-        else:
-            self.retries += 1
-            if self.max_retries and self.retries > self.max_retries:
-                Utils.reboot()
+        logging.info("%s: %s", __class__.__name__, message)
+        for size in ("large", "medium", "small"):
+            width, height = self.text_size(message, size, bold=True)
+            if width <= self.rect.width and height <= self.rect.height:
+                break
+        self.draw_text(message, (0, 0),
+                       size,
+                       "white",
+                       bold=True,
+                       align="center")
         self.update_screen(screen)
